9f190398f9ab4870470df1632218838c8344ba40
Bill Harding <bill@tahzoo.com>
2015-11-16 10:26:57 -0800

    Added first Udacity file

---
+++ Udacity_ML_nanoDegree.py
@@ -0,0 +1,35 @@
+myArray = ["offer",
+"is",
+"secret",
+"click",
+"secret",
+"link",
+"secret",
+"sport",
+"link",
+"play",
+"sport",
+"today",
+"went",
+"play",
+"sport",
+"secret",
+"sport",
+"event",
+"sport",
+"sport",
+"costs",
+"money"]
+
+'''What is the probabilty that a message will be in the spam bucket.
+SSSHHHHH or 11100000
+
+p(s) = pi
+p(Yi) = {pi if Yi = S,
+		1-pi if Yi = H}
+		
+p(Yi) = pi^Yi * (1-pi)^1-Yi
+
+
+'''
+		
